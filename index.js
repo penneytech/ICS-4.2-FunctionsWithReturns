222bbb130996d93252c6ffa70b216752ff2282f7
penneytech <99091373+penneytech@users.noreply.github.com>
2024-09-10 09:37:34 -0400

    First commit.

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+In this excercise you will combine two words together within a function. 
+
+1) Declare two varaibles for 'word1' and 'word2'. Set the initial values of each to a random word. 
+
+2) Create a function called 'wordCombine' with (word1, word2) as the parameters.
+
+3) Add the words together in the function and return the added value.
+
+4) Call the function in a console.log method that exists OUTSIDE of the function. This will print the return values of the function to the console. 
+*/
